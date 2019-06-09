511ec4a2283c3822ce75bb19813e630f241c381e
Hugo Fabricio <hugo@brzdigital.com.br>
2019-06-09 16:26:13 -0300

    Continue frontned

--- frontend/config/settings.js
+++ frontend/config/settings.js
@@ -1,23 +1,23 @@
-const path = require("path");
+const path = require('path')
 
-let ROOT = process.env.PWD;
+let ROOT = process.env.PWD
 
 if (!ROOT) {
-  ROOT = process.cwd();
+  ROOT = process.cwd()
 }
 
 const settings = {
-  appName: "FIAP Boilerplate",
-  favicon: path.join(ROOT, "/src/images/favicon.svg"),
-  devHost: "localhost",
+  appName: 'FIAP Boilerplate',
+  favicon: path.join(ROOT, '/src/images/favicon.svg'),
+  devHost: 'localhost',
   port: process.env.PORT || 8000,
   env: process.env.NODE_ENV,
   root: ROOT,
   paths: {
-    config: "config",
-    src: "src",
-    dist: "dist"
+    config: 'config',
+    src: 'src',
+    dist: '../public'
   }
-};
+}
 
-module.exports = settings;
+module.exports = settings
